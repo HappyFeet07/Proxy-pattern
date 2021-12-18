80ec55a3f1f297970ec2db0aeef2130078ac6a2c
Jack <jack0978239920@gmail.com>
2021-12-18 11:33:47 +0800

    remove IUpgradeable

--- contracts/inherit/Registry.sol
+++ contracts/inherit/Registry.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import './IRegistry.sol';
-import './IUpgradeable.sol';
 import './Upgradeable.sol';
 import './UpgradeabilityProxy.sol';
 
--- contracts/inherit/Upgradeable.sol
+++ contracts/inherit/Upgradeable.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.0;
 
 import './UpgradeabilityStorage.sol';
-import './IUpgradeable.sol';
 
-abstract contract Upgradeable is UpgradeabilityStorage, IUpgradeable {
-    function initialize(address sender) external virtual override payable {
+abstract contract Upgradeable is UpgradeabilityStorage {
+    function initialize(address sender) external virtual payable {
         require(sender == address(registry));
     }
 }
